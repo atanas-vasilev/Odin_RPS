e85d65f9761c2459869315cbd39e0d43d3b4e824
Atanas Vasilev <39429743+atanas-vasilev@users.noreply.github.com>
2022-03-02 09:23:07 +0000

    V2

--- app.js
+++ app.js
@@ -1,93 +1,123 @@
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
+document.querySelector("#start").addEventListener("click", init);
 
-function computerPlay(){
-    const rand = getRndInteger(1,3);
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+function computerChoice() {
+    const rand = getRndInteger(1, 3);
     console.log("Computer choice: " + rand);
-    switch(rand){
-        case 1: 
+    switch (rand) {
+        case 1:
             return ["ROCK", rand.toString()];
         case 2:
             return ["PAPER", rand.toString()];
         case 3:
             return ["SCISSORS", rand.toString()];
-        default:
-            console.error("Computer couldn't decide.")
     }
 }
-
-function playerSelection(){
-    //TO-DO: Error Handling
-    return prompt("Choose - Rock / Paper / Scissors").toLocaleUpperCase();
+function showPlayButtons() {
+    // show rock paper scissor btns and hide start button
+    resetGame();
+    document.querySelector("#start").style.display = "none";
+    document.querySelectorAll(".element").forEach(element => element.style.display = "flex");
+}
+function endGame() {
+    // hide rock paper scissor btns, show restart button
+    document.querySelector("#start").style.display = "flex";
+    document.querySelectorAll(".element").forEach(element => element.style.display = "none");
+}
+function resetGame() {
+    // hide rock paper scissor btnsl, reset points, show start button;
+    document.querySelector("#player-score").innerText = "0";
+    document.querySelector("#computer-score").innerText = "0";
+    document.querySelector("#current-round").innerText = "0";
+    document.querySelector("#log").innerHTML = "";
+    document.querySelector("#start").style.display = "flex";
+}
+function updateVisuals(ofWho, newScore, logText) {
+    //increment scrore visuals
+    if (ofWho == 'player') {
+        document.querySelector("#player-score").innerText = newScore;
+    } else if (ofWho == 'computer') {
+        document.querySelector("#computer-score").innerText = newScore;
+    }
+    //increment round visuals
+    document.querySelector("#current-round").innerText = Number(document.querySelector("#current-round").innerText)+1 ;
+    //log
+    document.querySelector("#log").innerHTML = `<li>${logText}</li> ${document.querySelector("#log").innerHTML}`;
 }
 
-function playRound(computerSelection, playerSelection){
+function playRound(playerSelection, computerSelection = computerChoice()) {
 
+    let playerScore = Number(document.querySelector("#player-score").innerText);
+    let computerScore = Number(document.querySelector("#computer-score").innerText);
+
+    let result;
     //If both are the same, no one wins.
     if (computerSelection[0] === playerSelection) {
-        return [0, `It's a tie. You both chose ${computerSelection[0]}`];
+        result = [0, `It's a tie. You both chose ${computerSelection[0]}`];
     }
 
     //If comp = rock
 
-    if (computerSelection[0] === "ROCK"){
-        if (playerSelection === "PAPER"){
-            return [1,`Player wins. ${playerSelection} beats ${computerSelection[0]}`];
-        } else if (playerSelection === "SCISSORS"){
-            return [2,`Computer wins. ${computerSelection[0]} beats ${playerSelection}`];
+    if (computerSelection[0] === "ROCK") {
+        if (playerSelection === "PAPER") {
+            result = [1, `Player wins. ${playerSelection} beats ${computerSelection[0]}`];
+        } else if (playerSelection === "SCISSORS") {
+            result = [2, `Computer wins. ${computerSelection[0]} beats ${playerSelection}`];
         }
     }
 
-    if (computerSelection[0] === "PAPER"){
-        if (playerSelection === "SCISSORS"){
-            return [1,`Player wins. ${playerSelection} beats ${computerSelection[0]}`];
-        } else if (playerSelection === "ROCK"){
-            return [2,`Computer wins. ${computerSelection[0]} beats ${playerSelection}`];
+    if (computerSelection[0] === "PAPER") {
+        if (playerSelection === "SCISSORS") {
+            result = [1, `Player wins. ${playerSelection} beats ${computerSelection[0]}`];
+        } else if (playerSelection === "ROCK") {
+            result = [2, `Computer wins. ${computerSelection[0]} beats ${playerSelection}`];
         }
     }
 
-    
-    if (computerSelection[0] === "SCISSORS"){
-        if (playerSelection === "ROCK"){
-            return [1,`Player wins. ${playerSelection} beats ${computerSelection[0]}`];
-        } else if (playerSelection === "PAPER"){
-            return [2,`Computer wins. ${computerSelection[0]} beats ${playerSelection}`];
+    if (computerSelection[0] === "SCISSORS") {
+        if (playerSelection === "ROCK") {
+            result = [1, `Player wins. ${playerSelection} beats ${computerSelection[0]}`];
+        } else if (playerSelection === "PAPER") {
+            result = [2, `Computer wins. ${computerSelection[0]} beats ${playerSelection}`];
         }
     }
-    
-}    
 
-function game(){
 
-    let playerScore = 0;
-    let computerScore = 0;
-
-    while(computerScore < 5 && playerScore < 5){
-
-        let result = playRound(computerPlay(), playerSelection())
-        alert(result[1]);
-
-        switch(result[0]){
-            case 0:
-                //tie, no one wins
-                console.log(`Tie, No one gets a point. PLAYER: ${playerScore} | COMPUTER: ${computerScore} `);
-                break;
-            case 1: 
-                //player wins
-                playerScore++;
-                console.log(`Player gets a point. Total: ${playerScore}`);
-                break;
-            case 2: 
-                //computer wins
-                computerScore++;
-                console.log(`Computer gets a point. Total: ${computerScore}`);
-                break;
-
-        }
-
+    switch (result[0]) {
+        case 0:
+            //tie, no one wins
+            console.log(`Tie, No one gets a point. PLAYER: ${playerScore} | COMPUTER: ${computerScore} `);
+            updateVisuals('none', 0, result[1]);
+            break;
+        case 1:
+            //player wins
+            playerScore++;
+            updateVisuals('player', playerScore, result[1]);
+            console.log(`Player gets a point. Total: ${playerScore}`);
+            break;
+        case 2:
+            //computer wins
+            computerScore++;
+            updateVisuals('computer', computerScore, result[1]);
+            console.log(`Computer gets a point. Total: ${computerScore}`);
+            break;
     }
 
-    alert(computerScore > playerScore ? "Computer Wins!!" : "Player Wins!!")
+    return result;
 
 }
+
+function init() {
+    showPlayButtons();
+
+    document.querySelector("#rock").addEventListener("click", () => { playRound("ROCK") });
+    document.querySelector("#paper").addEventListener("click", () => { playRound("PAPER") });
+    document.querySelector("#scissors").addEventListener("click", () => { playRound("SCISSORS") });
+
+
+    // endGame();
+}
+
+
